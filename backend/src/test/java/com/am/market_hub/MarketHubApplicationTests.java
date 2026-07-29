af6cc1eb5ff9d0378e2394f9b562dec8adfe5275
arkady-macuser <orkasha@gmail.com>
2026-07-29 11:57:10 +0300

    feat: initialize Spring Boot backend

---
+++ backend/src/test/java/com/am/market_hub/MarketHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.am.market_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
